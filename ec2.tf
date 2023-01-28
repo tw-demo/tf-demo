815e94af5eaf5b6aea1321fb60e6536474f86fa7
Raghu K <raghuk.vit@gmail.com>
2023-01-28 15:01:29 +0530

    Adding Security group

--- ec2.tf
+++ ec2.tf
@@ -2,7 +2,34 @@ resource "aws_instance" "i" {
   ami           = var.ami
   instance_type = var.instance_type
   key_name      = "sriharsha"
+  vpc_security_group_ids = [aws_security_group.allow_tls.id]
   tags = {
     Name = var.env
   }
 }
+
+resource "aws_security_group" "allow_tls" {
+  name        = "allow_tls"
+  description = "Allow TLS inbound traffic"
+  vpc_id      = "vpc-042992ad68653bd97"
+
+  ingress {
+    description      = "TLS from VPC"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow_tls"
+  }
+}
+
